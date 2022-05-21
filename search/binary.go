c60ebb36045aa089f8d2770ec70b2e97f1610f25
yujong-lee <yujonglee@g.skku.edu>
2022-05-21 13:45:09 +0900

    Add binaryRecursiveSearch

--- search/binary.go
+++ search/binary.go
@@ -23,3 +23,21 @@ func binaryLoopSearch(items []int, key int) (int, error) {
 
 	return -1, errors.New("NOT FOUND")
 }
+
+func binaryRecursiveSearch(items []int, key int, start int, end int) (int, error) {
+	if start > end {
+		return -1, errors.New("NOT FOUND")
+	}
+
+	mid := (start + end) / 2
+
+	if items[mid] > key {
+		return binaryRecursiveSearch(items, key, start, mid-1)
+	}
+
+	if items[mid] < key {
+		return binaryRecursiveSearch(items, key, mid+1, end)
+	}
+
+	return mid, nil
+}
--- search/binary_test.go
+++ search/binary_test.go
@@ -2,7 +2,7 @@ package search
 
 import "testing"
 
-func TestBinaryFound(t *testing.T) {
+func TestBinaryLoopFound(t *testing.T) {
 	index, error := binaryLoopSearch([]int{1, 2, 3, 5, 8, 11}, 8)
 
 	if index != 4 || error != nil {
@@ -10,8 +10,24 @@ func TestBinaryFound(t *testing.T) {
 	}
 }
 
-func TestBinaryNotFound(t *testing.T) {
+func TestBinaryLoopNotFound(t *testing.T) {
 	index, error := binaryLoopSearch([]int{1, 2, 3, 5, 8, 11}, 4)
+
+	if index != -1 || error == nil {
+		t.Error()
+	}
+}
+
+func TestBinaryRecursiveFound(t *testing.T) {
+	index, error := binaryRecursiveSearch([]int{1, 2, 3, 5, 8, 11}, 8, 0, 5)
+
+	if index != 4 || error != nil {
+		t.Error()
+	}
+}
+
+func TestBinaryRecursiveNotFound(t *testing.T) {
+	index, error := binaryRecursiveSearch([]int{1, 2, 3, 5, 8, 11}, 4, 0, 5)
 
 	if index != -1 || error == nil {
 		t.Error()
